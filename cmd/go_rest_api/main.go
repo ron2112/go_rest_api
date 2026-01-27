d7c9c0ae5130549e8bba54b431ec72b21b4b7f5c
cerpin_taxt <ghoshabhra5@gmail.com>
2026-01-27 11:51:30 +0530

    added get student by id

--- cmd/go_rest_api/main.go
+++ cmd/go_rest_api/main.go
@@ -28,6 +28,7 @@ func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /api/students", student.New(storage))
+	router.HandleFunc("GET /api/students/{id}", student.GetByID(storage))
 	// setup server
 	server := http.Server{
 		Addr:    cfg.Addr,
--- internal/http/student/student.go
+++ internal/http/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/ron2112/go_rest_api/internal/storage"
@@ -51,5 +52,27 @@ func New(storage storage.Storage) http.HandlerFunc {
 		}
 
 		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
+	}
+}
+
+func GetByID(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		slog.Info("getting a student", slog.String("id", id))
+	
+		intId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		student, err := storage.GetStudentByID(intId)
+		if err != nil {
+			slog.Error("Error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
+
 	}
 }
